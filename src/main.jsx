1917aeedc7909e847c03e7bb8900e76a4428c6ea
Heriberto <heribertolord@gmail.com>
2024-02-18 18:55:36 -0700

    git build

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 // import { store } from './store';
 import { App } from './App';
-// import MainContainers from './MainContainers';
-// import './main.css';
-// import 'sweetalert2/dist/sweetalert2.min.css';
-// import './locale/i18nextInit';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-//   <BrowserRouter>
       <App />
-//   {/* </BrowserRouter>, */}
-  // </React.StrictMode>,
 );
